fddd65e20ce6eda896ddb2d0ca27d65e790d2ef8
Jamie Smith <jeasmith@users.noreply.github.com>
2020-08-30 10:00:02 +0100

    Feature/app setup (#6)
    * OTHER: test components
    
    * OTHER: Added sass
    
    * OTHER: Added normalise
    
    * OTHER: Disallowed Cawling
    
    * OTHER: Fixed test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header, HeaderProps } from './components/header';
+import './App.scss';
+import { Footer, FooterProps } from './components/footer';
 
-function App() {
+function App(): JSX.Element {
+  const headerProps: HeaderProps = {
+    siteName: 'JamieDoesDev',
+    primaryLinks: [{text: 'link1', link: {href: 'https://example.com'}}]
+  };
+
+  const footerProps: FooterProps = {
+    title: 'Footer'
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header {...headerProps} />
+      
+      <Footer {...footerProps} />
     </div>
   );
 }
