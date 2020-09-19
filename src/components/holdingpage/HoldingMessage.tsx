ef20aa170d1a8717cf8a1d4b618831506d0ee138
Jamie Smith <jeasmith@users.noreply.github.com>
2020-09-19 15:00:05 +0100

    feature: added holding message (#8)

---
+++ src/components/holdingpage/HoldingMessage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss';
+
+const HoldingMessage: React.FunctionComponent = () => {
+    return ( 
+    <div className="holding-message">
+        <h1>Work in Progress</h1>
+        <h2>Bear with me, something good is on it's way</h2>
+    </div>
+    );
+}
+
+export default HoldingMessage;
---
+++ src/components/holdingpage/index.tsx
@@ -0,0 +1,3 @@
+import HoldingMessage from './HoldingMessage';
+
+export { HoldingMessage } ;
